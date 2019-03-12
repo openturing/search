537792216f3cd930d7d511604e47bdcd931990d8
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2019-03-12 19:11:38 -0300

    - GNU License Header

--- src/main/java/com/viglet/turing/api/sn/TurSNSiteAutoCompleteAPI.java
+++ src/main/java/com/viglet/turing/api/sn/TurSNSiteAutoCompleteAPI.java
@@ -1,12 +1,24 @@
+/*
+ * Copyright (C) 2016-2019 the original author or authors. 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.viglet.turing.api.sn;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +26,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.solr.client.solrj.response.SpellCheckResponse;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
